a8fede5c3f2bcf8ddffd1e8be11e41b0a66c90ff
aloisiot <loistc.carvalho@gmail.com>
2021-07-29 15:59:35 -0300

    reorganizando

--- poo/class.js
+++ poo/class.js
@@ -2,16 +2,16 @@
 
 // Declareção da classe Livro
 // Essa maneira de declaração de classes foi introduzida a partir do ES2015.
-class Livros{
+class Livros {
 
     // Função construtora que será invocada ao instanciar um novo objeto da classe Livros.
-    constructor(nome, paginas, isbn){
-        this.nome=nome;
-        this.paginas=paginas;
-        this.isbn=isbn;
+    constructor(nome, paginas, isbn) {
+        this.nome = nome;
+        this.paginas = paginas;
+        this.isbn = isbn;
     }
 
-    printInfo(){ // imprime as informações a cerca do livro
+    printInfo() { // imprime as informações a cerca do livro
         console.log(`
         Nome: ${this.nome};
         Páginas: ${this.paginas};
@@ -21,19 +21,19 @@ class Livros{
 }
 
 // Instanciaçãos da classe Livros
-const sapiens=new Livros('Uma breve história da humanidade', 591, '978-85-254-3461-6');
+const sapiens = new Livros('Uma breve história da humanidade', 591, '978-85-254-3461-6');
 
 sapiens.printInfo();// Invocação do método
 
 
 
 // Há também outra maneira de declarar uma classe, como feito abaixo
-function Filmes(titulo, diretor, duracao){
-    this.titulo=titulo;
-    this.duracao=duracao;
-    this.diretor=diretor;
-    
-    this.printInfo=()=>{ // Imprime as informaçoes a cerca do livro
+function Filmes(titulo, diretor, duracao) {
+    this.titulo = titulo;
+    this.duracao = duracao;
+    this.diretor = diretor;
+
+    this.printInfo = () => { // Imprime as informaçoes a cerca do livro
         console.log(`
         Titulo: ${this.titulo};
         Diretor: ${this.diretor};
@@ -43,6 +43,6 @@ function Filmes(titulo, diretor, duracao){
 }
 
 // Instanciaçãos da classe Filmes
-const avatar=new Filmes('Avatar','James Cameron', 9720);
+const avatar = new Filmes('Avatar', 'James Cameron', 9720);
 
 avatar.printInfo();// Invocação do metodo criado anteriormente
--- poo/heranca.js
+++ poo/heranca.js
@@ -1,24 +1,36 @@
 // Herançã é um pilar principal na Programação Orientada a Objetos
 
-class Livros{
+class Livros {
 
-    constructor(name, pages, isbn){
-        this.name=name;
-        this.pages=pages;
-        this.isbn=isbn;
+    constructor(name, paginas, isbn) {
+        this.name = name;
+        this.paginas = paginas;
+        this.isbn = isbn;
     }
 
-    printIsbn(){
+    printIsbn() {
         console.log(this.isbn);
     }
 }
 
 // Estendendo a class Livros.
-class LivrosTI extends Livros{
-    constructor(name, pages, isbn, tecnologia){
-        super(name, pages, isbn); // a função super() instancia os atributos da classe estendida.
+class LivrosTI extends Livros {
+    constructor(name, paginas, isbn, tecnologia) {
+        super(name, paginas, isbn); // a função super() instancia os atributos da classe estendida.
 
-        this.tecnologia=tecnologia; // atributo declaradopara a nova classe.
+        this.tecnologia = tecnologia; // atributo declaradopara a nova classe.
     }
+
+    // Sobrescrita do metodo printInfo()
+    printInfo() { // imprime as informações a cerca do livro
+        console.log(`
+        Nome: ${this.nome};
+        Páginas: ${this.paginas};
+        Isbn: ${this.isbn};
+        Tecnologia: ${this.tecnologia}.
+        `);
+    }
+
 }
 
+const java = new LivrosTI('Java, como programar', 934, '978-85-430-0479-2', 'Java')
